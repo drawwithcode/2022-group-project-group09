98b8eb3dddcd1c7387bb9c53215ce0b20b90ba11
SofiaCadisco99 <113481279+SofiaCadisco99@users.noreply.github.com>
2023-01-30 14:10:56 +0100

    sketch prova

--- discover1.js
+++ discover1.js
@@ -1,12 +1,16 @@
 const urlString = window.location.href; // Collect the URL string of the page
 let url = new URL(urlString); // Convert it into a parsable URL Object
-
+let img;
 let container, button, counterP;
 
 let mask;
 
+function preload() {
+  img = loadImage("./assets/sketch1.png");
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, 4000);
   mask = createBlurMask(500, 500, 30, 25);
 
   container = createElement("div");
@@ -25,10 +29,10 @@ function setup() {
 }
 
 function draw() {
-  background(250, 200, 40);
-  fill(0);
-  textSize(100);
-  text("discover1", 200, 200);
+  image(img, 0, 0, windowWidth, 4000);
+  // fill(0);
+  // textSize(100);
+  // text("discover1", 200, 200);
   applyMask(mask, mouseX - mask.width / 2, mouseY - mask.height / 2);
 }
 
